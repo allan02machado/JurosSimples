34dbce7b33a8e1ee3543057aee8e99f935b29e39
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-25 02:09:50 -0300

    Este commit altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor;
+            int meses;
+            double juros;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Calcule o juros final de um investimento");
+            Console.ResetColor();
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.WriteLine();
+            Console.ReadKey();
+
+            Console.Write("Entre com o valor do capital R$: ");
+            valor = double.Parse(Console.ReadLine());
+
+            Console.Write("Insira a quantidade de meses: ");
+            meses = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira a taxa de juros: ");
+            juros = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            double total2 = (valor * juros) * meses / 100;
+            Console.WriteLine($"Juros: {total2:C}");
+
+            double total = valor + ((meses * (juros / 100)) * valor);
+            Console.WriteLine($"Montante: {total:C}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # JurosSimples
 Resultado da atividade "JurosSimples "
+
+**Enunciado**
+
+Calcule o montante final de um investimento a juros simples.
+
+**Como executar o programa**
+
+Abra o programa e digite:
+
+dotnet run
+
